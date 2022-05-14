c4e4274ee530ae95efb73fe4d3faf7923d3f9fcd
Denj1X <white.hollow.ro@gmail.com>
2022-05-14 23:53:06 +0300

    Implementarea claselor

---
+++ Cont.h
@@ -0,0 +1,11 @@
+#ifndef OOP_CONT_H
+#define OOP_CONT_H
+
+#include <bits/stdc++.h>
+
+class Cont {
+
+};
+
+
+#endif //OOP_CONT_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!\n";
+    std:: cout << 5;
     return 0;
 }
